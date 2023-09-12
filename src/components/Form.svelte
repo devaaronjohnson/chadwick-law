a242815aadb6faa52a0007196ac77ff08cb1d15a
Aaron Johnson <aaronjohnsondev@gmail.com>
2023-09-11 19:07:49 -0500

    landing page dev

---
+++ src/components/Form.svelte
@@ -0,0 +1,109 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { fade } from 'svelte/transition';
+
+	// Booleans
+	let showServiceList = false;
+
+	// Variables
+	let name,
+		phone,
+		email,
+		message,
+		service = '';
+
+	let nameClicked,
+		phoneClicked,
+		emailClicked,
+		serviceClicked = '';
+
+	function submitForm() {}
+</script>
+
+<div class="relative bg-white p-10 rounded-3xl shadow-2xl">
+	<h3 class="text-center font-oregon font-medium text-3xl text-black mb-3">Get Started</h3>
+	<p class="text-center font-montserrat font-light text-base text-black mb-5">
+		Lorem, ipsum dolor sit amet consectetur adipisicing elit. At, optio! Quaerat vitae animi
+		recusandae sint.
+	</p>
+	<form on:submit|preventDefault={submitForm} class="relative">
+		<div class="w-full mb-3 relative">
+			<label for="name"> Name </label>
+			<input
+				id="name"
+				class="border border-gray-300 rounded p-3 font-montserrat font-light text-base w-full"
+				placeholder="Name"
+				bind:value={name}
+				on:focus={() => (nameClicked = true)}
+			/>
+		</div>
+		<div class="w-full mb-3 relative">
+			<label for="phone"> Phone </label>
+			<input
+				id="phone"
+				class="border border-gray-300 rounded p-3 font-montserrat font-light text-base w-full"
+				placeholder="Phone"
+				bind:value={phone}
+				on:focus={() => (phoneClicked = true)}
+			/>
+		</div>
+		<div class="w-full mb-3 relative">
+			<label for="Email"> Phone </label>
+			<input
+				id="email"
+				class="border border-gray-300 rounded p-3 font-montserrat font-light text-base w-full"
+				placeholder="email"
+				bind:value={email}
+				on:focus={() => (emailClicked = true)}
+			/>
+		</div>
+		<div class="w-full mb-3 relative">
+			<label for="relative service"> Service </label>
+			<input
+				id="service"
+				class="relative border border-gray-300 rounded p-3 font-montserrat font-light text-base w-full"
+				placeholder="Service"
+				bind:value={service}
+				on:focus={() => {
+					serviceClicked = true;
+					showServiceList = true;
+				}}
+			/>
+			{#if showServiceList}
+				<div
+					class="w-full border border-red-300 bg-red-100 rounded p-5 absolute top-16 left-0 w-full shadow-xl z-10"
+				>
+					<ul>
+						<li>
+							<a href=".">Link</a>
+						</li>
+						<li>
+							<a href=".">Link</a>
+						</li>
+					</ul>
+				</div>
+			{/if}
+		</div>
+		<div class="w-full mb-3 relative">
+			<label for="message"> Message </label>
+			<textarea
+				rows="4"
+				id="message"
+				class="border border-gray-300 rounded p-3 font-montserrat font-light text-base w-full"
+				placeholder="Message"
+				bind:value={message}
+			/>
+		</div>
+		<button
+			class="w-full font-montserrat font-normal uppercase text-white text-base rounded px-8 py-3 border border-black bg-black"
+		>
+			Submit
+		</button>
+	</form>
+</div>
+
+<style>
+	label {
+		@apply hidden;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+	// Components
+	import { Form } from '$components';
+
 	import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
 	// Default theme
 	import '@splidejs/svelte-splide/css';
@@ -11,28 +14,33 @@
 	<title>{pageTitle}</title>
 </svelte:head>
 
-<section class="bg-white pt-40 pb-40">
-	<div class="w-full max-w-screen-xl mx-auto">
-		<h1 class="font-oregon font-medium text-black text-6xl w-1/2">
-			Legal expertise without the headaches.
-		</h1>
-		<p class="font-montserrat font-normal text-black text-xl w-1/2 mb-3">
-			Legal services do not need to be complicated or expensive. Let me help walk you through your
-			legal needs in a timely manner at a reasonable fee.
-		</p>
-		<a
-			href="."
-			class="border border-brandGreen bg-transparent font-montserrat font-medium text-brandGreen text-base px-10 py-3 rounded-full transition-all duration-300 hover:bg-brandGreen hover:text-black inline-block"
-			>Get Started</a
-		>
+<section class="bg-gray-300 pt-0 pb-0">
+	<div class="w-full max-w-screen-xl mx-auto flex flex-row items-center">
+		<div class="w-1/2">
+			<h1 class="font-oregon font-medium text-black text-6xl">
+				Legal expertise without the headaches.
+			</h1>
+			<p class="font-montserrat font-normal text-black text-xl mb-3">
+				Legal services do not need to be complicated or expensive. Let me help walk you through your
+				legal needs in a timely manner at a reasonable fee.
+			</p>
+			<div class="flex justify-center lg:justify-start items-center">
+				<a
+					href="."
+					class="border border-black bg-black font-montserrat font-medium text-white text-base px-10 py-3 rounded-full transition-all duration-300 hover:bg-brandGreen hover:text-black inline-block"
+					>Get Started</a
+				>
+			</div>
+		</div>
+		<div class="flex-1 pl-20">
+			<div class="translate-y-20"><Form /></div>
+		</div>
 	</div>
 </section>
 
-<section class="bg-white pt-28 pb-28">
+<section class="bg-white pt-32 pb-32">
 	<div class="w-full max-w-screen-xl mx-auto">
-		<h2 class="font-oregon font-medium text-black text-4xl mb-12 text-center">
-			What I Can Help You With
-		</h2>
+		<h2 class="font-oregon font-medium text-black text-5xl mb-12">What I Can Help You With?</h2>
 		<div class="grid grid-cols-3 gap-12">
 			<div class="bg-gray-200 p-12 rounded-xl">
 				<div class="flex justify-center items-center mb-5">
@@ -148,14 +156,14 @@
 	</div>
 </section>
 
-<section class="bg-white pt-40 pb-40">
+<section class="bg-gray-100 pt-32 pb-32">
 	<div class="w-full max-w-screen-xl mx-auto">
 		<div class="grid grid-cols-2 gap-12">
 			<div>
 				<div class="flex justify-center items-center mb-5">Image Here</div>
 			</div>
 			<div>
-				<h3 class="font-oregon font-medium text-black text-3xl mb-3">About Connie</h3>
+				<h3 class="font-oregon font-medium text-black text-5xl mb-3">About Connie</h3>
 				<p class="font-montserrat font-normal text-black text-lg mb-4">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequatur quia
 					aspernatur eaque optio officiis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
@@ -188,7 +196,7 @@
 
 <section class="bg-white pt-28 pb-28">
 	<div class="w-full max-w-screen-xl mx-auto">
-		<h2 class="font-oregon font-medium text-black text-4xl mb-12 text-center">
+		<h2 class="font-oregon font-medium text-black text-5xl mb-12 text-center">
 			What people are saying
 		</h2>
 
@@ -196,7 +204,7 @@
 			options={{
 				type: 'loop',
 				perPage: 1,
-				height: '100px',
+				height: '200px',
 				perMove: 1,
 				arrows: true,
 				padding: '0rem'
@@ -205,45 +213,44 @@
 			<SplideSlide>
 				<div class="testimonial">
 					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequatur quia
-						aspernatur eaque optio officiis.
+						I found her to be compassionate to my needs and concerns, timely and efficient, fair in
+						pricing with a true desire to settle as fairly and quickly as possible without selling
+						you out. She'll fight your cause. She is knowledgeable in ethics and procedures, with
+						true concerns on how your situation is affecting you.
 					</p>
-					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						- Lorem ipsum
-					</p>
+					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">- Mike</p>
 				</div>
 			</SplideSlide>
 			<SplideSlide>
 				<div class="testimonial">
 					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequatur quia
-						aspernatur eaque optio officiis.
+						Connie Chadwick goes over and above her line of duty! She is very thorough in what she
+						does and is always prepared. She relates to you on a personal level, not just a client.
+						My opinion would be if you are looking for this character, and intellect she is the one!
 					</p>
-					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						- Lorem ipsum
-					</p>
+					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">- Dawn</p>
 				</div>
 			</SplideSlide>
 			<SplideSlide>
 				<div class="testimonial">
 					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequatur quia
-						aspernatur eaque optio officiis.
+						I hired conine Chadwick to work on my civil case. Connie has a drive to perform the task
+						at hand. Works hard to achieve the best possible results. Communication is open, work
+						quick response. Truthful with the facts. I'm very happy with Connie Chadwick and will
+						always reference her to handle any other matters that may arise.
 					</p>
-					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						- Lorem ipsum
-					</p>
+					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">- Andrew</p>
 				</div>
 			</SplideSlide>
 			<SplideSlide>
 				<div class="testimonial">
 					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet consequatur quia
-						aspernatur eaque optio officiis.
+						Connie did an amazing job of walking me through the difficult process of my divorce. She
+						was very knowledgable and offered great advice. I could not imagine things going more
+						smoothly. She was professional, trustworthy. I would recommend her to anyone facing the
+						same ordeal.
 					</p>
-					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">
-						- Lorem ipsum
-					</p>
+					<p class="font-montserrat font-normal text-black text-lg mb-4 text-center">- Aaron</p>
 				</div>
 			</SplideSlide>
 		</Splide>
@@ -254,7 +261,7 @@
 	.testimonial {
 		width: 100%;
 		height: 100%;
-		padding: 20px 40px;
+		padding: 20px 200px;
 	}
 	:global(.splide__slide) {
 		width: 100% !important;
