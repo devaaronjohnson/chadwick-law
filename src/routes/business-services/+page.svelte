e3186dc2897166cb53f5ff026d78e2e2e330d4fb
Aaron Johnson <aaronjohnsondev@gmail.com>
2023-09-17 18:37:39 -0500

    business services pages

--- src/routes/business-services/+page.svelte
+++ src/routes/business-services/+page.svelte
@@ -2,6 +2,7 @@
 	import { Form } from '$components';
 	import { animateScroll } from 'svelte-scrollto-element';
 
+	// Variables
 	let services = ['DBA', 'LLC', 'Inc'];
 
 	// Meta
@@ -13,7 +14,7 @@
 	<title>{pageTitle}</title>
 </svelte:head>
 
-<section class="bg-gray-300 pt-0 b-40">
+<!-- <section class="bg-gray-300 pt-0 b-40">
 	<div class="w-full max-w-screen-xl mx-auto flex flex-row items-center">
 		<div class="w-1/2">
 			<h1 class="font-oregon font-medium text-5xl text-black mb-6">Get down to business</h1>
@@ -28,30 +29,57 @@
 				startup costs are often tax deductible.
 			</p>
 		</div>
-		<div class="flex-1 pl-28">
+		<div id="form" class="flex-1 pl-28">
+			<div class="translate-y-20"><Form {services} /></div>
+		</div>
+	</div>
+</section> -->
+
+<section class="relative bg-brandDarkBlue pt-32 lg:pt-16 pb-0">
+	<img src="/ccl_name.svg" alt="" class="bg_name" />
+	<div class="relative container_xl container_padding flex flex-col lg:flex-row items-center">
+		<div class="relative w-full lg:w-1/2">
+			<h1 class="font-oregon font-medium text-white text-5xl lg:text-6xl mb-6">
+				Get down to business
+			</h1>
+			<p class="font-montserrat font-normal text-white text-xl mb-3">
+				There’s more than one way to officially set up a business. We’re here to help make sense of
+				these different structures so you can start on the right path—or scale to the next level.
+				We’ll check if your company name is available, and file all the required forms. Together,
+				let’s make your business official.
+			</p>
+			<div class="flex justify-start lg:justify-start items-center">
+				<a
+					on:click|preventDefault={() =>
+						animateScroll.scrollTo({ element: '#services', duration: 1000, offset: -140 })}
+					href="."
+					class="btn_hero border border-brandOrange bg-brandOrange text-white hover:bg-brandOrange hover:text-white"
+					>View Services</a
+				>
+			</div>
+		</div>
+		<div class="relative w-full lg:flex-1 pl-0 lg:pl-28">
 			<div class="translate-y-20"><Form {services} /></div>
 		</div>
 	</div>
 </section>
 
-<section class="pt-28 pb-28">
-	<div class="w-full max-w-screen-xl mx-auto">
-		<h2 class="font-oregon font-medium text-4xl text-black w-1/2">
+<section class="pt-44 lg:pt-32 pb-12 lg:pb-28">
+	<div class="container_xl container_padding">
+		<h2 class="font-oregon font-medium text-3xl lg:text-4xl text-black w-full lg:w-1/2">
 			Choose what is right for your business.
 		</h2>
-		<p class="font-montserrat font-normal text-black text-base text-left mb-3">
+		<p class="font-montserrat font-normal text-black text-lg lg:text-base text-left mb-3">
 			Decide what happens to your property and who raises your children.
 		</p>
-		<div class="grid grid-cols-3 gap-5 mt-10">
+		<div id="services" class="grid grid-cols-1 lg:grid-cols-3 gap-5 mt-10">
 			<div class="shadow-xl p-12 rounded-xl bg-white">
 				<!-- <i class="fa-regular fa-house-heart text-4xl mb-5" /> -->
 				<h3 class="font-oregon font-medium text-black text-3xl text-left mb-3">LLC</h3>
 				<p class="font-montserrat font-medium text-black text-base text-left">
 					Limited Liability Company
 				</p>
-				<p class="font-montserrat font-normal text-black text-sm text-left">
-					Starts at $0 + filing fees
-				</p>
+
 				<p class="font-montserrat font-normal text-black text-sm text-left mb-3">
 					Start up costs are often tax deductible
 				</p>
@@ -59,17 +87,15 @@
 					Going solo or teaming up? Make sure you're not on the hook for business liabilities with
 					an LLC. Get yours started for free—just pay state filing fees.
 				</p>
-				<p class="font-montserrat font-bold text-black text-lg mb-4 text-left">
-					From $299 + filing fees
-				</p>
+				<p class="font-montserrat font-medium text-black text-3xl text-left mb-4">$89</p>
+
 				<div class="flex justify-start items-center">
 					<a
 						href="/business-services/llc"
-						class="inline-block border border-black bg-transparent rounded-full px-8 py-2 font-montserrat font-medium text-sm"
-						>Start my LLC</a
+						class="btn_sm border border-brandOrange bg-brandOrange text-white">Start my LLC</a
 					>
 				</div>
-				<div class="mt-6">
+				<!-- <div class="mt-6">
 					<p class="font-montserrat font-bold text-base">How it's unique</p>
 					<ul class="list-disc pl-6">
 						<li class="font-montserrat font-normal text-base">
@@ -101,31 +127,25 @@
 							Not recognized globally; you may be taxed as a corporation in other countries
 						</li>
 					</ul>
-				</div>
+				</div> -->
 			</div>
 			<div class="shadow-xl p-12 rounded-xl bg-white">
 				<!-- <i class="fa-regular fa-house-heart text-4xl mb-5" /> -->
 				<h3 class="font-oregon font-medium text-black text-3xl text-left mb-3">DBA</h3>
 				<p class="font-montserrat font-medium text-black text-base text-left">Doing Business As</p>
-				<p class="font-montserrat font-normal text-black text-sm text-left mb-3">
-					Starts at $99 + filing fees
-				</p>
 
 				<p class="font-montserrat font-normal text-black text-base text-left mb-3">
 					Go by a different business name without creating a new company. Over half of our DBA
 					customers are sole proprietors.
 				</p>
-				<p class="font-montserrat font-bold text-black text-lg mb-4 text-left">
-					From $99 + filing fees
-				</p>
+				<p class="font-montserrat font-medium text-black text-3xl text-left mb-4">$89</p>
 				<div class="flex justify-start items-center">
 					<a
 						href="/business-services/dba"
-						class="inline-block border border-black bg-transparent rounded-full px-8 py-2 font-montserrat font-medium text-sm"
-						>Start my DBA</a
+						class="btn_sm border border-brandOrange bg-brandOrange text-white">Start my DBA</a
 					>
 				</div>
-				<div class="mt-6">
+				<!-- <div class="mt-6">
 					<p class="font-montserrat font-bold text-base">How it's unique</p>
 					<ul class="list-disc pl-6">
 						<li class="font-montserrat font-normal text-base">
@@ -157,35 +177,29 @@
 							Not recognized globally; you may be taxed as a corporation in other countries
 						</li>
 					</ul>
-				</div>
+				</div> -->
 			</div>
 
 			<div class="shadow-xl p-12 rounded-xl bg-white">
 				<!-- <i class="fa-regular fa-house-heart text-4xl mb-5" /> -->
-				<h3 class="font-oregon font-medium text-black text-3xl text-left">INC</h3>
-				<p class="font-montserrat font-medium text-black text-base text-left">
-					Corperation (S or C Corp)
-				</p>
-				<p class="font-montserrat font-normal text-black text-sm text-left">
-					Starts at $149 + filing fees
-				</p>
+				<h3 class="font-oregon font-medium text-black text-3xl text-left">Corporation</h3>
+				<p class="font-montserrat font-medium text-black text-base text-left">(S or C Corp)</p>
+
 				<p class="font-montserrat font-normal text-black text-sm text-left mb-3">
 					Start up costs are often tax deductible
 				</p>
 				<p class="font-montserrat font-normal text-black text-base text-left mb-3">
 					Plan to issue shares, go public, or go global? Go further as a corporation.
 				</p>
-				<p class="font-montserrat font-bold text-black text-lg mb-4 text-left">
-					From $149 + filing fees
-				</p>
+				<p class="font-montserrat font-medium text-black text-3xl text-left mb-4">$89</p>
 				<div class="flex justify-start items-center">
 					<a
-						href="/business-services/inc"
-						class="inline-block border border-black bg-transparent rounded-full px-8 py-2 font-montserrat font-medium text-sm"
-						>Start my Inc</a
+						href="/business-services/corporation"
+						class="btn_sm border border-brandOrange bg-brandOrange text-white"
+						>Start my Corporation</a
 					>
 				</div>
-				<div class="mt-6">
+				<!-- <div class="mt-6">
 					<p class="font-montserrat font-bold text-base">How it's unique</p>
 					<ul class="list-disc pl-6">
 						<li class="font-montserrat font-normal text-base">
@@ -217,8 +231,18 @@
 							Not recognized globally; you may be taxed as a corporation in other countries
 						</li>
 					</ul>
-				</div>
+				</div> -->
 			</div>
 		</div>
 	</div>
 </section>
+
+<style>
+	.bg_name {
+		position: absolute;
+		right: 100px;
+		bottom: 0;
+		opacity: 0.1;
+		height: 200px;
+	}
+</style>
