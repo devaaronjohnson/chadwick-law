74cc5b3c8399828f35baf5572ab37668a7184d78
Aaron Johnson <aaronjohnsondev@gmail.com>
2023-09-19 19:44:20 -0500

    page dev nav and footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,13 +1,18 @@
-<footer class="bg-black pt-12 lg:pt-20 pb-12">
+<footer class="bg-white pt-12 lg:pt-20 pb-12">
 	<div class="container_xl container_padding">
 		<div class="flex flex-col lg:flex-row items-start">
-			<div class="flex-1 mb-4 lg:mb-0">
-				<p class="font-montserrat font-medium text-white text-xl lg:text-base">Estate Planning</p>
+			<div class="flex-none mb-4 lg:mb-0 pr-20">
+				<a href="/">
+					<img src="/ccl_logo2.png" alt="Connie Chadwick Law" class="h-16 lg:h-16" />
+				</a>
+			</div>
+			<div class="w-1/3 mb-4 lg:mb-0">
+				<p class="font-montserrat font-bold text-black text-xl lg:text-base">Estate Planning</p>
 				<ul>
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-lg lg:text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-lg lg:text-sm transition-all duration-300 hover:underline"
 						>
 							Last Will
 						</a>
@@ -15,7 +20,7 @@
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-lg lg:text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-lg lg:text-sm transition-all duration-300 hover:underline"
 						>
 							Living Trust
 						</a>
@@ -23,7 +28,7 @@
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-lg lg:text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-lg lg:text-sm transition-all duration-300 hover:underline"
 						>
 							Living Will
 						</a>
@@ -31,20 +36,20 @@
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-lg lg:text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-lg lg:text-sm transition-all duration-300 hover:underline"
 						>
 							Power of Attorney
 						</a>
 					</li>
 				</ul>
 			</div>
-			<div class="flex-1 mb-4 lg:mb-0">
-				<p class="font-montserrat font-medium text-white text-xl lg:text-base">Business Services</p>
+			<div class="w-1/3 mb-4 lg:mb-0">
+				<p class="font-montserrat font-bold text-black text-xl lg:text-base">Business Services</p>
 				<ul>
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-lg lg:text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-lg lg:text-sm transition-all duration-300 hover:underline"
 						>
 							LLC
 						</a>
@@ -52,7 +57,7 @@
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-lg lg:text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-lg lg:text-sm transition-all duration-300 hover:underline"
 						>
 							DBA
 						</a>
@@ -60,74 +65,36 @@
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-lg lg:text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-lg lg:text-sm transition-all duration-300 hover:underline"
 						>
 							Inc
 						</a>
 					</li>
 				</ul>
 			</div>
-			<div class="flex-1">
-				<p class="font-montserrat font-bold text-white text-base">Family</p>
-				<ul>
-					<li>
-						<a
-							href="."
-							class="font-montserrat font-normal text-white text-sm transition-all duration-300 hover:underline"
-						>
-							Divorce
-						</a>
-					</li>
-					<li>
-						<a
-							href="."
-							class="font-montserrat font-normal text-white text-sm transition-all duration-300 hover:underline"
-						>
-							Name Change
-						</a>
-					</li>
-				</ul>
+			<div class="w-2/5">
+				<p class="font-montserrat font-bold text-black text-base">Real Estate</p>
+				<p class="font-montserrat font-bold text-black text-base">Divorce</p>
+				<p class="font-montserrat font-bold text-black text-base">Human Resources & Labor Issues</p>
+				<p class="font-montserrat font-bold text-black text-base">Contract Consultation</p>
 			</div>
-			<div class="w-1/3">
-				<p class="font-montserrat font-bold text-white text-base">Follow</p>
+
+			<div class="pl-10 w-1/4">
+				<p class="font-montserrat font-bold text-black text-base">Follow</p>
 				<ul class="flex flex-row items-center">
 					<li>
 						<a
 							href="."
-							class="font-montserrat font-normal text-white text-sm transition-all duration-300 hover:underline"
+							class="font-montserrat font-normal text-black text-sm transition-all duration-300 hover:underline"
 						>
 							Social
-						</a>
-					</li>
-					<li>
-						<a
-							href="."
-							class="font-montserrat font-normal text-white text-sm transition-all duration-300 hover:underline"
-						>
-							Trust
-						</a>
-					</li>
-					<li>
-						<a
-							href="."
-							class="font-montserrat font-normal text-white text-sm transition-all duration-300 hover:underline"
-						>
-							Something Else
-						</a>
-					</li>
-					<li>
-						<a
-							href="."
-							class="font-montserrat font-normal text-white text-sm transition-all duration-300 hover:underline"
-						>
-							Something else
 						</a>
 					</li>
 				</ul>
 			</div>
 		</div>
 		<div class="mt-10">
-			<p class="font-montserrat font-normal text-sm text-center text-white opacity-50">
+			<p class="font-montserrat font-normal text-sm text-center text-black opacity-50">
 				&copy; 2023 Connie Chadwick Law. All Rights Reserved
 			</p>
 		</div>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,8 +4,7 @@
 	import { quintOut } from 'svelte/easing';
 
 	//Booleans
-	let showPersonalMenu = false;
-	let showBusinessMenu = false;
+	let showMenu = false;
 
 	// Booleans
 	let showMobileNav = false;
@@ -22,8 +21,8 @@
 		? 'bg-transparent shadow-none'
 		: 'bg-white opacity-[0.98] shadow-md'} px-5"
 >
-	<div class="container_xl">
-		<div class="flex flex-row items-center justify-between">
+	<div class="relative container_xl">
+		<div class="relative flex flex-row items-center justify-between">
 			<div class="flex-1">
 				{#if y < 30}
 					<a href="/">
@@ -35,34 +34,83 @@
 					</a>
 				{/if}
 			</div>
-			<div class="flex-1 hidden lg:block">
-				<ul class="flex items-center justify-end">
-					<li>
+			<div class="relative flex-1 hidden lg:block">
+				<ul class="relative flex items-center justify-end">
+					<li use:clickOutside on:click_outside={() => (showMenu = false)} class="relative">
 						<a
-							on:click|preventDefault={() => (showBusinessMenu = true)}
+							on:click|preventDefault={() => (showMenu = !showMenu)}
 							href="."
 							class={y < 30 ? 'text-white' : 'text-black'}
 						>
-							Business <i
-								class="fa-light fa-angle-down {y < 30 ? 'text-white' : 'text-black'}"
+							Services <i
+								class="transition-all duration-500 fa-light fa-angle-down {y < 30
+									? 'text-white'
+									: 'text-black'} {showMenu ? 'rotate-180' : 'rotate-0'}"
 							/></a
 						>
+						{#if showMenu}
+							<div
+								transition:fade={{ duration: 300 }}
+								class="bg-white rounded-xl p-5 shadow-xl absolute top-10 left-0 w-[230px]"
+							>
+								<ul>
+									<li class="text-sm leading-relaxed">
+										<a
+											class="text-black hover:text-brandOrange transition-all duration-300 font-normal"
+											href="/estate-planning"
+										>
+											Estate Planning
+										</a>
+									</li>
+									<li class="text-sm leading-relaxed">
+										<a
+											class="text-black hover:text-brandOrange transition-all duration-300 font-normal"
+											href="/business-services"
+										>
+											Business Services
+										</a>
+									</li>
+									<li class="text-sm leading-relaxed">
+										<a
+											class="text-black hover:text-brandOrange transition-all duration-300 font-normal"
+											href="/family"
+										>
+											Family
+										</a>
+									</li>
+									<li class="text-sm leading-relaxed">
+										<a
+											class="text-black hover:text-brandOrange transition-all duration-300 font-normal"
+											href="/real-estate"
+										>
+											Real Estate
+										</a>
+									</li>
+									<li class="text-sm leading-relaxed">
+										<a
+											class="text-black hover:text-brandOrange transition-all duration-300 font-normal"
+											href="/about"
+										>
+											Human Resources & Labor Issues
+										</a>
+									</li>
+									<li class="text-sm leading-relaxed">
+										<a
+											class="text-black hover:text-brandOrange transition-all duration-300 font-normal"
+											href="/about"
+										>
+											Contract Consultation
+										</a>
+									</li>
+								</ul>
+							</div>
+						{/if}
 					</li>
-					<li class="pl-10">
-						<a
-							on:click|preventDefault={() => (showPersonalMenu = true)}
-							href="."
-							class={y < 30 ? 'text-white' : 'text-black'}
-						>
-							Personal <i
-								class="fa-light fa-angle-down {y < 30 ? 'text-white' : 'text-black'}"
-							/></a
-						>
-					</li>
-					<li class="pl-10">
+
+					<li class="pl-10 text-base ml-6">
 						<a href="/about" class={y < 30 ? 'text-white' : 'text-black'}> About </a>
 					</li>
-					<li class="pl-10">
+					<li class="pl-10 text-base ml-6">
 						<a href="/contact" class={y < 30 ? 'text-white' : 'text-black'}> Contact </a>
 					</li>
 				</ul>
@@ -76,329 +124,8 @@
 	</div>
 </nav>
 
-{#if showPersonalMenu}
-	<div
-		in:fade={{ duration: 300 }}
-		use:clickOutside
-		on:click_outside={() => (showPersonalMenu = false)}
-		class="w-full absolute top-16 left-0 w-full bg-white pt-10 pb-10 shadow-xl"
-	>
-		<div class="w-full max-w-screen-xl mx-auto">
-			<div class="grid grid-cols-4 gap-10">
-				<div>
-					<p class="font-montserrat font-bold text-base mb-3">
-						<a on:click={() => (showPersonalMenu = false)} href="/personal/estate-planning"
-							>Estate Planning</a
-						>
-					</p>
-					<ul>
-						<li>
-							<a
-								on:click={() => (showPersonalMenu = false)}
-								href="/personal/estate-planning/last-will"
-							>
-								Last will
-							</a>
-						</li>
-						<li>
-							<a
-								on:click={() => (showPersonalMenu = false)}
-								href="/personal/estate-planning/living-trust"
-							>
-								Living trust
-							</a>
-						</li>
-						<li>
-							<a
-								on:click={() => (showPersonalMenu = false)}
-								href="/personal/estate-planning/power-of-attorney"
-							>
-								Power of attorney
-							</a>
-						</li>
-						<li>
-							<a
-								on:click={() => (showPersonalMenu = false)}
-								href="/personal/estate-planning/living-will"
-							>
-								Living will
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<p class="font-montserrat font-bold text-base mb-3">
-						<a on:click={() => (showPersonalMenu = false)} href="/personal/family">Family</a>
-					</p>
-					<ul>
-						<li>
-							<a on:click={() => (showPersonalMenu = false)} href="/personal/family/divorce">
-								Divorce
-							</a>
-						</li>
-						<!-- <li>
-							<a
-								on:click={() => (showPersonalMenu = false)}
-								href="/personal/estate-planning/living-trust"
-							>
-								Name Change
-							</a>
-						</li> -->
-					</ul>
-				</div>
-				<div>
-					<p class="font-montserrat font-bold text-base mb-3">
-						<a on:click={() => (showPersonalMenu = false)} href="/personal/family">Real Estate</a>
-					</p>
-					<ul>
-						<li>
-							<a
-								on:click={() => (showPersonalMenu = false)}
-								href="/personal/estate-planning/last-will"
-							>
-								Coming soon!
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-{/if}
-
-{#if showBusinessMenu}
-	<div
-		in:fade={{ duration: 300 }}
-		use:clickOutside
-		on:click_outside={() => (showBusinessMenu = false)}
-		class="w-full absolute top-16 left-0 w-full bg-white pt-10 pb-10 shadow-xl"
-	>
-		<div class="w-full max-w-screen-xl mx-auto">
-			<div class="grid grid-cols-4 gap-10">
-				<div>
-					<p class="font-montserrat font-bold text-base mb-3">
-						<a on:click={() => (showBusinessMenu = false)} href="/business-formation"
-							>Business Formation</a
-						>
-					</p>
-					<ul>
-						<li>
-							<a on:click={() => (showBusinessMenu = false)} href="/business-formation/llc">
-								Limited liability company (LLC)
-							</a>
-						</li>
-						<li>
-							<a on:click={() => (showBusinessMenu = false)} href="/business-formation/inc">
-								Corporation (C corp, S corp)
-							</a>
-						</li>
-						<li>
-							<a on:click={() => (showBusinessMenu = false)} href="/business-formation/dba">
-								Doing business as (DBA)
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<p class="font-montserrat font-bold text-base mb-3">
-						<a on:click={() => (showBusinessMenu = false)} href="/personal/family">Family</a>
-					</p>
-					<ul>
-						<li>
-							<a
-								on:click={() => (showBusinessMenu = false)}
-								href="/personal/estate-planning/last-will"
-							>
-								Divorce
-							</a>
-						</li>
-						<li>
-							<a
-								on:click={() => (showBusinessMenu = false)}
-								href="/personal/estate-planning/living-trust"
-							>
-								Name Change
-							</a>
-						</li>
-					</ul>
-				</div>
-				<div>
-					<p class="font-montserrat font-bold text-base mb-3">
-						<a on:click={() => (showBusinessMenu = false)} href="/personal/family">Real Estate</a>
-					</p>
-					<ul>
-						<li>
-							<a
-								on:click={() => (showBusinessMenu = false)}
-								href="/personal/estate-planning/last-will"
-							>
-								Coming soon!
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-{/if}
-
-{#if showMobileNav}
-	<div
-		in:fly={{ duration: 750, x: 1000, easing: quintOut, opacity: 1 }}
-		out:fade={{ duration: 300 }}
-		class="fixed top-0 left-0 bottom-0 w-full bg-black z-50 px-8 pt-28 overflow-y-scroll"
-	>
-		<a href="." on:click|preventDefault={() => (showMobileNav = false)}>
-			<i class="fa-regular fa-xmark text-4xl text-white absolute top-5 right-5" />
-		</a>
-		<ul class="relative flex flex-col items-end w-full">
-			<li
-				class="font-oregon font-medium text-white text-3xl py-3 transition-all duration-200 hover:text-brandRed text-right"
-			>
-				<a
-					on:click={() => {
-						animateScroll.scrollTo({ element: '#top', duration: 1000, offset: -150 });
-						showMobileNav = false;
-					}}
-					href="."
-				>
-					Home
-				</a>
-			</li>
-			<li
-				class="font-oregon font-medium text-white text-3xl py-3 transition-all duration-200 hover:text-brandRed text-right"
-			>
-				<a
-					on:click={() => {
-						animateScroll.scrollTo({ element: '#serviceList', duration: 1000, offset: -150 });
-						showMobileNav = false;
-					}}
-					href="."
-				>
-					Estate Planning
-				</a>
-				<ul class="mt-2">
-					<li class="font-montserrat font-normal text-white text-lg text-right">
-						<a
-							href="/estate-planning/last-will"
-							class="text-white opacity-50 transition-all duration-200 hover:opacity-100"
-							>Last Will</a
-						>
-					</li>
-					<li class="font-montserrat font-normal text-white text-lg text-right">
-						<a
-							href="/estate-planning/living-trust"
-							class="text-white opacity-50 transition-all duration-200 hover:opacity-100"
-							>Living Trust</a
-						>
-					</li>
-					<li class="font-montserrat font-normal text-white text-lg text-right">
-						<a
-							href="/estate-planning/living-will"
-							class="text-white opacity-50 transition-all duration-200 hover:opacity-100"
-							>Living Will</a
-						>
-					</li>
-					<li class="font-montserrat font-normal text-white text-lg text-right">
-						<a
-							href="/estate-planning/power-of-attorney"
-							class="text-white opacity-50 transition-all duration-200 hover:opacity-100"
-							>Power of Attorney</a
-						>
-					</li>
-				</ul>
-			</li>
-			<li
-				class="font-oregon font-medium text-white text-3xl py-3 transition-all duration-200 hover:text-white text-right"
-			>
-				<a
-					on:click={() => {
-						animateScroll.scrollTo({ element: '#pricing', duration: 1000, offset: -130 });
-						showMobileNav = false;
-					}}
-					href="."
-				>
-					Business Services
-				</a>
-				<ul class="mt-2">
-					<li class="font-montserrat font-normal text-white text-lg text-right">
-						<a
-							href="/business-services/dba"
-							class="text-white opacity-50 transition-all duration-200 hover:opacity-100">DBA</a
-						>
-					</li>
-					<li class="font-montserrat font-normal text-white text-lg text-right">
-						<a
-							href="/business-services/inc"
-							class="text-white opacity-50 transition-all duration-200 hover:opacity-100">Inc</a
-						>
-					</li>
-					<li class="font-montserrat font-normal text-white text-lg text-right">
-						<a
-							href="/business-services/llc"
-							class="text-white opacity-50 transition-all duration-200 hover:opacity-100">LLC</a
-						>
-					</li>
-				</ul>
-			</li>
-			<li
-				class="font-oregon font-medium text-white text-3xl py-3 transition-all duration-200 hover:text-white text-right"
-			>
-				<a
-					on:click={() => {
-						animateScroll.scrollTo({ element: '#form', duration: 1000, offset: -50 });
-						showMobileNav = false;
-					}}
-					href="."
-				>
-					Family
-				</a>
-			</li>
-			<li
-				class="font-oregon font-medium text-white text-3xl py-3 transition-all duration-200 hover:text-white text-right"
-			>
-				<a
-					on:click={() => {
-						animateScroll.scrollTo({ element: '#form', duration: 1000, offset: 730 });
-						showMobileNav = false;
-					}}
-					href="."
-				>
-					Real Estate
-				</a>
-			</li>
-			<li
-				class="font-oregon font-medium text-white text-3xl py-3 transition-all duration-200 hover:text-white text-right"
-			>
-				<a
-					on:click={() => {
-						animateScroll.scrollTo({ element: '#form', duration: 1000, offset: 730 });
-						showMobileNav = false;
-					}}
-					href="."
-				>
-					Human Resources & Labor Issues
-				</a>
-			</li>
-			<li
-				class="font-oregon font-medium text-white text-3xl py-3 transition-all duration-200 hover:text-white text-right"
-			>
-				<a
-					on:click={() => {
-						animateScroll.scrollTo({ element: '#form', duration: 1000, offset: 730 });
-						showMobileNav = false;
-					}}
-					href="."
-				>
-					Contract Consultation
-				</a>
-			</li>
-		</ul>
-	</div>
-{/if}
-
 <style>
-	#nav li {
-		@apply font-montserrat font-normal text-base ml-10;
+	li {
+		@apply font-montserrat font-normal;
 	}
 </style>
