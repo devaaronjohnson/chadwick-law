bef85a45279a437d55485910f78bcbdb6defee3f
Aaron Johnson <aaronjohnsondev@gmail.com>
2023-09-20 19:57:27 -0500

    page dev

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header.svelte';
 import Footer from './Footer.svelte';
 import Form from './Form.svelte';
+import Testimonials from './Testimonials.svelte';
 
-export { Header, Footer, Form };
+export { Header, Footer, Form, Testimonials };
